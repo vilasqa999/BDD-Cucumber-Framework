77779496b89f1748324b5f91a5c9786a0b3674f1
Vilas Giri <138094708+vilasqa999@users.noreply.github.com>
2023-08-20 18:48:26 +0530

    updated logic

--- src/test/java/PageObject/test.java
+++ src/test/java/PageObject/test.java
@@ -7,7 +7,14 @@ public class test {
 		// TODO Auto-generated method stub
 
 		System.out.println("hello");
+
+		int a=10;
+		int b=20;
+		int sum=0;
+		sum=a+b;
 		
+		System.out.println("hello sum is " + sum);
+
 	}
 
 }
